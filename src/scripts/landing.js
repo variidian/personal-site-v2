461fc43d744bf3ff60bdf0310b0d741075f2ab7f
turdle <shuruiguo1@gmail.com>
2026-09-14 18:38:06 +1000

    hoverable sections!!!!

---
+++ src/scripts/landing.js
@@ -0,0 +1,32 @@
+
+// artist section 
+document.getElementById('artist_sec').addEventListener('mouseenter', () => {
+    document.getElementById('artist_outline').classList.remove('hidden')
+  });
+  document.getElementById('artist_sec').addEventListener('mouseleave', () => {
+    document.getElementById('artist_outline').classList.add('hidden')
+  });
+
+// programmer section 
+document.getElementById('programmer_sec').addEventListener('mouseenter', () => {
+    document.getElementById('programmer_outline').classList.remove('hidden')
+  });
+  document.getElementById('programmer_sec').addEventListener('mouseleave', () => {
+    document.getElementById('programmer_outline').classList.add('hidden')
+  });
+
+// wip section 
+document.getElementById('wip_sec').addEventListener('mouseenter', () => {
+    document.getElementById('wip_outline').classList.remove('hidden')
+  });
+  document.getElementById('wip_sec').addEventListener('mouseleave', () => {
+    document.getElementById('wip_outline').classList.add('hidden')
+  });
+
+// variidian section 
+document.getElementById('variidian_sec').addEventListener('mouseenter', () => {
+    document.getElementById('variidian_outline').classList.remove('hidden')
+  });
+  document.getElementById('variidian_sec').addEventListener('mouseleave', () => {
+    document.getElementById('variidian_outline').classList.add('hidden')
+  });
